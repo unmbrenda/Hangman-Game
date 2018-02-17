fc81b6dd7b216eb6db83e32935cdf4454833bac1
unmbrenda <brenda.l.bassett@gmail.com>
2018-02-16 18:33:56 -0600

    added words

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -24,68 +24,90 @@ function isAlphaNum(char) {
     }
 }
 
-var lives = 10
-var myWord = "maine-coon"
+var lives = 10;
+var myWord = "maine-coon";
+var wordList = ["maine-coon", "whiskers", "burmese"];
+
 document.onkeyup = function (event) {
-    var letter = event.key.toLowerCase(); //gives letter pushed, convert to lower case, assign result to var letter
-    if (isAlphaNum(letter)) { //checks to see if alphanumeric using previous function above, will only execute what is in the curlies if the expression in the parentheses is true
-        var n = myWord.indexOf(letter);//looks to see if letter pressed is in word
-        if (n >= 0) {//if not in word returns -1, finds index of letter in word
-            // happy path: only if n >=0 will these lines be executed
-            for (var i = 0; i < myWord.length; i++) {//sets # of times to loop and ++ means loop again if condition not met
-                if (myWord.charAt(i) === letter) {//if letter typed is letter in word
-                    document.getElementById("w0-l" + i).innerHTML = letter;//if letter chosen is in word, it will display
-                    if (isWordGuessed(myWord)=== true){//if returns value of true, then show pic
-                        showPic();
-                    }
-                    
-                }
-                
-            }
-        }
-        else {
-            // sad path
-            
-            var failedGuesses = document.getElementById("guessed").innerHTML;
-            
-            if (failedGuesses.indexOf(letter) === -1) {
-                document.getElementById("guessed").innerHTML = failedGuesses + letter;    
-                lives--;  
+    if (isWordGuessed(myWord) || lives <= 0) {
+        var wordIndex = wordList.indexOf(myWord);
+        if (wordIndex < wordList.length) {
+            lives = 10;
+            hidePic(myWord);
+            hideWord(myWord);
+            myWord = wordList[wordIndex + 1];
+            showWord(myWord);
+            document.getElementById("guessed").innerHTML = "";
             document.getElementById("remaining").innerHTML = lives;
-                 
-            }
             
-            if (lives <= 0) {
-                // picture
-                showPic();
+        }
+    }
+    else {
+
+        var letter = event.key.toLowerCase(); //gives letter pushed, convert to lower case, assign result to var letter
+        if (isAlphaNum(letter)) { //checks to see if alphanumeric using previous function above, will only execute what is in the curlies if the expression in the parentheses is true
+            var n = myWord.indexOf(letter);//looks to see if letter pressed is in word
+            if (n >= 0) {//if not in word returns -1, finds index of letter in word
+                // happy path: only if n >=0 will these lines be executed
+                for (var i = 0; i < myWord.length; i++) {//sets # of times to loop and ++ means loop again if condition not met
+                    if (myWord.charAt(i) === letter) {//if letter typed is letter in word
+                        document.getElementById("w" + wordList.indexOf(myWord) + "-l" + i).innerHTML = letter;//if letter chosen is in word, it will display
+                        if (isWordGuessed(myWord) === true) {//if returns value of true, then show pic
+                            showPic(myWord);
+                        }
+                    }
+                }
+            }
+            else {
+                // sad path
+
+                var failedGuesses = document.getElementById("guessed").innerHTML;
+
+                if (failedGuesses.indexOf(letter) === -1) {
+                    document.getElementById("guessed").innerHTML = failedGuesses + letter;
+                    lives--;
+                    document.getElementById("remaining").innerHTML = lives;
+                }
+                if (lives <= 0) {
+                    // picture
+                    showPic(myWord);
+                }
             }
         }
     }
 }
-
-function showPic() {
-    var picture = document.getElementById("maine-coon-pic");
+function showPic(word) {
+    var picture = document.getElementById(word + "-pic");
     picture.classList.remove("hidden");
 
     var stats = document.getElementById("stats");
     stats.classList.add("hidden");
-
 }
+function hidePic(word) {
+    var picture = document.getElementById(word + "-pic");
+    picture.classList.add("hidden");
 
+    var stats = document.getElementById("stats");
+    stats.classList.remove("hidden");
+}
 function isWordGuessed(word) {
     var letterHolder = document.getElementById(word);
 
+
     for (var i = 0; i < letterHolder.childElementCount; i++) {
-        if (letterHolder.children[i].innerText.length === 0){
+        if (letterHolder.children[i].innerText.length === 0) {
             return false;
         }
 
-       
-    }
-            return true;
-    }
 
-    // $("#roastbeef").on("click", function(){
-    //     trackSandwiches++;
-    //     alert("You have eaten " + trackSandwiches + " sandwiches");
-    //   });
+    }
+    return true;
+}
+function hideWord(word) {
+    var theWord = document.getElementById(word);
+    theWord.classList.add("hidden");
+}
+function showWord(word) {
+    var theWord = document.getElementById(word);
+    theWord.classList.remove("hidden");
+} 
